ea357e8eb301615afee0d519312ce25cbffd53f3
sarahwoolsey87 <shedontusejelly0@gmail.com>
2023-08-08 15:49:00 +0100

    got stuff working omg

--- client/src/App.js
+++ client/src/App.js
@@ -1,4 +1,5 @@
-import { useState, useEffect } from 'react';
+import React, { useState, useEffect } from "react";
+import axios from "axios";
 
 const API_BASE = "http://localhost:8091";
 
@@ -8,18 +9,42 @@ function App() {
   const [newTodo, setNewTodo] = useState("");
 
   useEffect(() => {
-    GetTodos();
+    getTodos();
   }, []);
 
-  const GetTodos = () => {
-    fetch(API_BASE + "/todos")
-      .then(res => res.json())
-      .then(data => {
-        setTodos(data);
-        console.log(data); 
-      })
-      .catch(err => console.error("Error: ", err));
-  }
+  const getTodos = async () => {
+    const API = API_BASE + "/todos";
+    const res = await axios.get(API);
+    setTodos(res.data);
+  };
+
+  const completeTodo = async (id) => {
+    const API = API_BASE + "/todo/complete/" + id;
+    const res = await axios.put(API);
+    getTodos();
+  };
+
+  const deleteTodo = async (id) => {
+    const API = API_BASE + "/todo/delete/" + id;
+    const res = await axios.delete(API);
+    getTodos();
+  };
+
+  const addTodo = async () => {
+    const data = await fetch(API_BASE + "/todo/new", {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify({
+        text: newTodo,
+      }),
+    }).then((res) => res.json());
+
+    setTodos([...todos, data]);
+    setPopupActive(false);
+    setNewTodo("");
+  };
 
   return (
     <div className="App">
@@ -27,19 +52,46 @@ function App() {
       <h4>Your Tasks</h4>
 
       <div className="Lots of todos">
-        {todos.map(todo => (
-          <div className={
-            "todo" + (todo.complete ? "is-complete" : "")
-        } key={todo._id}>
-
+        {todos.map((todo) => (
+          <div
+            className={"todo " + (todo.complete ? "is-complete" : "")}
+            key={todo._id}
+          >
             <div className="checkbox"></div>
-
-            <div className="text">{todo.text}</div>
-
-            <div className="delete-todo">x</div>
+            <div className="text" onClick={() => completeTodo(todo._id)}>
+              {todo.text}
+            </div>
+            <div className="delete-todo" onClick={() => deleteTodo(todo._id)}>
+              x
+            </div>
           </div>
         ))}
       </div>
+      <div className="addPopup" onClick={() => setPopupActive(true)}>
+        +
+      </div>
+      {popupActive ? (
+        <div className="popup">
+          <div className="closePopup" onClick={() => setPopupActive(false)}>
+            x
+          </div>
+          <div className="content">
+            <h3>Add Task</h3>
+            {newTodo}
+            <input
+              type="text"
+              className="add-todo-input"
+              onChange={(e) => setNewTodo(e.target.value)}
+              value={newTodo}
+            />
+            <div className="button" onClick={addTodo}>
+              Create Task
+            </div>
+          </div>
+        </div>
+      ) : (
+        ""
+      )}
     </div>
   );
 }
--- server/server.js
+++ server/server.js
@@ -1,6 +1,6 @@
-const express = require('express');
-const mongoose = require('mongoose');
-const cors = require('cors');
+const express = require("express");
+const mongoose = require("mongoose");
+const cors = require("cors");
 require("dotenv").config();
 const app = express();
 
@@ -8,39 +8,40 @@ app.use(express.json());
 app.use(cors());
 
 //Connect to MongoDB & CL
-mongoose.connect(process.env.DATABASE_URL, {
-  useNewUrlParser: true,
-  useUnifiedTopology: true,
-})
-.then(() => {
-  console.log('Connected to MongoDB');
-})
-.catch(error => {
-  console.error('Error connecting to MongoDB:', error.message);
-});
+mongoose
+  .connect(process.env.DATABASE_URL, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+  })
+  .then(() => {
+    console.log("Connected to MongoDB");
+  })
+  .catch((error) => {
+    console.error("Error connecting to MongoDB:", error.message);
+  });
 
 const PORT = process.env.PORT || 8091;
 
-const Todo = require('./models/Todo');
+const Todo = require("./models/Todo");
 
-app.get('/todos', async (req, res) => {
+app.get("/todos", async (req, res) => {
   const todos = await Todo.find();
   res.json(todos);
 });
 
 app.post("/todo/new", (req, res) => {
   const todo = new Todo({
-    text: req.body.text
+    text: req.body.text,
   });
 
-  todo.save(); 
+  todo.save();
 
   res.json(todo);
 });
 
 app.delete("/todo/delete/:id", async (req, res) => {
   const result = await Todo.findByIdAndDelete(req.params.id);
-
+  console.log("id", req.params.id);
   if (!result) {
     return res.status(404).json({ message: "Todo not found" });
   }
@@ -50,14 +51,13 @@ app.delete("/todo/delete/:id", async (req, res) => {
 
 app.put("/todo/complete/:id", async (req, res) => {
   const todo = await Todo.findById(req.params.id);
-
+  console.log("putid", req.params.id);
   todo.complete = !todo.complete;
 
   todo.save();
 
   res.json(todo);
-
-})
+});
 
 app.listen(PORT, () => {
   console.log(`Server started on port ${PORT}`);
